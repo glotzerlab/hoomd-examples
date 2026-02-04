6b241e8c1503247acfa054c749ff8e38675cd9af
Kate Jensen <kmjens@umich.edu>
2026-02-04 15:53:19 -0500

    precommit and ruff

--- 11-Active-Particles-In-MD/utility.py
+++ 11-Active-Particles-In-MD/utility.py
@@ -1,11 +1,12 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def fibonacci_sphere(num_pts, R):
     '''
-    Returns points on the surface of a sphere. 
+    Returns points on the surface of a sphere.
     num_pts is the number of points on the sphere.
-    R is the radius of my desired mesh. 
+    R is the radius of my desired mesh.
     '''
 
     indices = np.arange(0, num_pts, dtype=float) + 0.5
@@ -23,21 +24,21 @@ def get_active_pos_in_spherical(num_points, radius):
     #First generate random points in spherical coords and then convert to cartesian.
 
     # Radii:
-    r = np.random.uniform(0, radius ** (1/3), num_points) 
-    
+    r = np.random.uniform(0, radius ** (1/3), num_points)
+
     # Polar angles (theta):
     theta = np.random.uniform(0, 2 * np.pi, num_points)
-    
+
     # Azimuthal angles (phi):
     phi = np.arccos(2 * np.random.uniform(0, 1, num_points) - 1)
-    
+
     # Spherical coordinates to Cartesian coordinates
     x = r * np.sin(phi) * np.cos(theta)
     y = r * np.sin(phi) * np.sin(theta)
     z = r * np.cos(phi)
 
     points = np.vstack((x, y, z)).T
-    
+
     return points
 '''
 
@@ -49,20 +50,20 @@ def get_active_pos_A(rho, radius):
         x = np.linspace(-radius+(sigma/2), radius-(sigma/2), num_points)
         y = np.random.uniform(-radius+(sigma/2), radius-(sigma/2), num_points)
         z = np.random.uniform(-radius+(sigma/2), radius-(sigma/2), num_points)
-        
+
         #Calculate the distance of the points from the origin
         distances = np.sqrt(x**2 + y**2 + z**2)
-        
+
         # Select points that lie within the sphere
         mask = distances <= radius
         selected_points = np.vstack((x[mask], y[mask], z[mask])).T
-        
+
         # Add selected points to the list
         points.extend(selected_points)
         points = np.array(points)
-    
+
     #points = np.array(points[:num_points]) # Trim to the desired number of points
-    
+
     return points
 
 def calc_tether_bond_lengths(fmin,fmax,fc1,fc0,k_b):
@@ -71,7 +72,7 @@ def calc_tether_bond_lengths(fmin,fmax,fc1,fc0,k_b):
     This function is not optimized right now -- work needs to be done to determine how to do this the best.
     '''
     l_min = global_min_edge*fmin
-    l_max = global_max_edge*fmax 
+    l_max = global_max_edge*fmax
     l_c1 = global_min_edge*fc1
     l_c0 = global_max_edge*fc0
 
@@ -82,7 +83,7 @@ def calc_tether_bond_lengths(fmin,fmax,fc1,fc0,k_b):
     print('l_c1: ',l_c1)
     print('l_min: ',l_min)
     print('k_bond: ',k_b)
-    
+
     return(l_min,l_max,l_c1,l_c0)
 
 
@@ -96,17 +97,17 @@ def find_U_tether_bond(r,tether_bond_params):
     l_c0 = tether_bond_params[2]
     l_max = tether_bond_params[3]
     k_bond = tether_bond_params[4]
-    
+
     if r > l_c0:
         U_att = (k_bond*np.exp(1/(l_c0-r))) / (l_max-r)
     elif r <= l_c0:
         U_att = 0
-    
+
     if r < l_c1:
         U_rep = (k_bond*np.exp(1/(r-l_c1))) / (r-l_min)
     elif r >= l_c1:
         U_rep = 0
-    
+
     U_tot = U_att + U_rep
 
     return(U_att, U_rep, U_tot)
@@ -140,7 +141,7 @@ def plot_tether_energies(tether_bond_params):
     plt.ylabel('Potential')
     plt.title('Mesh Bond Potential vs. Distance r')
     plt.legend()
-    plt.gca().set_facecolor('white') 
+    plt.gca().set_facecolor('white')
     plt.grid(True, color='lightgrey', linestyle =':')
     plt.axvline(x=l_min, color='grey',linestyle ='--',linewidth=1,label='l_min')
     plt.axvline(x=l_max, color='grey',linestyle ='--',linewidth=1,label='l_max')
@@ -153,7 +154,7 @@ def plot_tether_energies(tether_bond_params):
     plt.xlim(l_min-0.1,l_max+0.1)
     plt.ylim(-5,max(U_tot_values)+10)
     plt.show()
-    
+
 '''
 def plot_tether_energies():
     plt.figure(figsize=(5, 4))
@@ -164,7 +165,7 @@ def plot_tether_energies():
     plt.ylabel('Potential')
     plt.title('Mesh Bond Potential vs. Distance r')
     plt.legend()
-    plt.gca().set_facecolor('white') 
+    plt.gca().set_facecolor('white')
     plt.grid(True, color='lightgrey', linestyle =':')
     plt.axvline(x=l_min, color='grey',linestyle ='--',linewidth=1,label='l_min')
     plt.axvline(x=l_max, color='grey',linestyle ='--',linewidth=1,label='l_max')
@@ -177,7 +178,7 @@ def plot_tether_energies():
     plt.xlim(l_min-0.1,l_max+0.1)
     plt.ylim(-5,max(U_tot_values)+10)
     plt.show()
-'''   
+'''
 
 def find_mesh_radius_avg_and_std(sim):
     '''
@@ -185,10 +186,10 @@ def find_mesh_radius_avg_and_std(sim):
     Outputs:
         AVGrad: average radius of flexicle if assume spherical.
         STDrad: standard deviation of mesh points from the avg radius value.
-    '''   
+    '''
     snap = sim.state.get_snapshot()
-    pos = snap.particles.position    
-    
+    pos = snap.particles.position
+
     rad = np.zeros(len(pos))
     for j in range(len(pos)):
         for i in list([0,1,2]):
